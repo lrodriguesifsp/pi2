c6b7734d681e1ded10fa058c8bffa4c4826d8707
Luiz Rodrigues <lrodrigues@ifsp.edu.br>
2023-10-08 20:57:34 -0300

    linhas routes

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var linhasRouter = require('./routes/linhas');
 
 var app = express();
 
@@ -16,5 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/linhas', linhasRouter);
 
 module.exports = app;
